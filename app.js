07efc780fc77d80f5be44faf5b62f63e17473db8
Bruno Guimarães <devgmrs@gmail.com>
2022-05-31 19:38:53 -0300

    alterações para serem desfeitas

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('olá, mundo');
+const nomeUsuario = 'bruno cesar de souza guimaraes';
+
+const comprimentoDoNome = nomeUsuario.split(' ');
+const nomeSemEspaco = comprimentoDoNome.join('');
+
+console.log(nomeSemEspaco);
