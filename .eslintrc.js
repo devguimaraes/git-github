eada186edc10895cb33c4a97d494815a4fe00de3
Bruno Guimarães <devgmrs@gmail.com>
2022-05-31 18:44:34 -0300

    Alterando as regrar do eslitn e criando titulo no html

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["prettier"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    camelcase: "off",
-    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['airbnb-base', 'prettier'],
+	plugins: ['prettier'],
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'no-console': 'off',
+		'prettier/prettier': 'error',
+		'class-methods-use-this': 'off',
+		'no-param-reassign': 'off',
+		camelcase: 'off',
+		'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+	},
 };
